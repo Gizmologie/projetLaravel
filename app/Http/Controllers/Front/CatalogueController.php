fc898da76802aeed07287d098fbdaca882a8d346
benjamin <emailpourfb90@gmail.com>
2020-10-20 15:03:11 +0200

    pagination

--- app/Http/Controllers/Front/CatalogueController.php
+++ app/Http/Controllers/Front/CatalogueController.php
@@ -100,7 +100,7 @@ class CatalogueController extends Controller
         $this->results = $this->results->where('stock_quantity', '>', 0);
 
         $this->results = $this->results->orderBy('price');
-        $this->results = $this->results->paginate(9);
+        $this->results = $this->results->paginate(9)->appends(request()->except('page'));
     }
 
 }
